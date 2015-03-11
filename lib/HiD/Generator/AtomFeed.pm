2685b11edd7281e222659e586430f16d7881f175
John SJ Anderson <genehack@genehack.org>
2015-03-11 14:10:53 -0700

    Update AtomFeed plugin
    * reorganize config keys
    * remove unused attribute
    * improve documentation

--- lib/HiD/Generator/AtomFeed.pm
+++ lib/HiD/Generator/AtomFeed.pm
@@ -4,12 +4,36 @@
 
 This Generator produces an Atom feed of your posts.
 
-Enable it by setting the 'atom_feed' key in your config to the path where the
-feed should be generated.
+Enable it by setting the 'atom_feed.generate' key in your config to 1. Also
+set an 'atom_feed.destination' key indicating where the feed should be
+generated.
 
-The 'atom_feed_base' and 'atom_feed_link' keys, if they exist, will be used to
-add the 'rel=alternate' base link and the 'rel=self' feed link to the atom
-feed, respectively.
+You may also set optional 'atom_feed.base' and 'atom_feed.link' keys to set
+the 'rel=alternate' base link and the 'rel=self' link to the atom feed,
+respectively.
+
+The 'atom.title' config key can be used to give the feed a title. Otherwise
+the site title will be used.
+
+The 'atom.posts' config key can be used to control the number of posts in the
+feed. It defaults to 20.
+
+=head2 DEPRECATED CONFIG
+
+This module formerly used different config keys. If this older configuration
+is detected, a warning will be emitted and the configuration will be
+internally upgraded to the new format. If you wish to silence this warning,
+convert your config to use these new keys:
+
+    OLD                   NEW
+    atom_feed ----------> atom_feed.generate
+                    *AND* atom_feed.destination
+    atom_feed_base  ----> atom_feed.base
+    atom_feed_link  ----> atom_feed.link
+    atom_feed_posts ----> atom_feed.posts
+    atom_feed_title ----> atom_feed.title
+
+See the documentation above to understand what each key controls.
 
 =cut
 
@@ -25,11 +49,6 @@ use XML::Atom::Feed;
 use XML::Atom::Link;
 use XML::Atom::Person;
 
-has 'destination' => (
-  is      => 'ro' ,
-  isa     => 'HiD_DirPath' ,
-);
-
 =method generate
 
 =cut
@@ -37,11 +56,17 @@ has 'destination' => (
 sub generate {
   my( $self , $site ) = @_;
 
-  return unless
-    my $destination = $site->config->{atom_feed};
+  if ( exists $site->config->{atom_feed} && ref $site->config->{atom_feed} ne 'HASH' ){
+    $site->WARN( "Using deprecated atom_feed config! Please see docs on how to update!" );
 
-  my $post_limit = $site->config->{atom_feed_posts} // 20;
-  my $post_count = 1;
+    $site->config->{atom_feed} = _upgrade_atom_config( $site->config );
+  }
+
+  return unless $site->config->{atom_feed}{generate};
+
+  my $destination = $site->config->{atom_feed}{destination};
+  my $post_limit  = $site->config->{atom_feed}{posts} // 20;
+  my $post_count  = 1;
 
   my $feed = $self->_new_feed($site);
 
@@ -86,11 +111,11 @@ sub _new_entry {
 sub _new_feed {
   my( $self , $site ) = @_;
 
-  my $feed = XML::Atom::Feed->new();
-  my $title = $site->config->{atom_feed_title} // $site->config->{title};
+  my $feed  = XML::Atom::Feed->new();
+  my $title = $site->config->{atom_feed}{title} // $site->config->{title};
   $feed->title( $title );
 
-  if ( my $base_url = $site->config->{atom_feed_base} ) {
+  if ( my $base_url = $site->config->{atom_feed}{base} ) {
     my $base_link = XML::Atom::Link->new();
     $base_link->type('text/html');
     $base_link->rel('alternate');
@@ -99,7 +124,7 @@ sub _new_feed {
     $feed->add_link( $base_link );
   }
 
-  if ( my $feed_url = $site->config->{atom_feed_link} ) {
+  if ( my $feed_url = $site->config->{atom_feed}{link} ) {
     my $feed_link = XML::Atom::Link->new();
     $feed_link->type('application/atom+xml');
     $feed_link->rel('self');
@@ -113,6 +138,24 @@ sub _new_feed {
   $feed->updated(DateTime->now());
 
   return $feed;
+}
+
+
+sub _upgrade_atom_config {
+  my( $old_config ) = @_;
+
+  my $new_config = {
+    generate    => 1 ,
+    destination => $old_config->{atom_feed}
+  };
+
+  foreach (qw/ base link posts title /) {
+    if ( exists $old_config->{"atom_feed_$_"} ) {
+      $new_config->{$_} = $old_config->{"atom_feed_$_"}
+    };
+  }
+
+  return $new_config;
 }
 
 __PACKAGE__->meta->make_immutable;
